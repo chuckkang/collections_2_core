ce49f27fdac0a5068576249baef888bf3d4d8ef1
Chuck Kang <chuckkang@gmail.com>
2018-02-26 12:23:04 -0800

    simple arrays

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _2_collectionspractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+			// create an array to hold integer values 0-9
+			int[] int1 = new int[10];
+			for (int x=0; x<10; x++){
+				int1[x]=x;
+				Console.WriteLine(int1[x]);
+			}
+			// create a array of the names time,  martin, nikki sara
+			string[] arrstr = {"Tim", "martin", "nikki", "sara"};
+			foreach(string str in arrstr ){
+				Console.WriteLine(str);
+			}
+
+			// create an array of 10 that alternates between true and false values, starting with true
+			bool[] arrBool = new bool[10];
+			bool val = true;
+			for (int i=0; i < arrBool.Length; i++){
+				if (i%2==0){
+					val = true;
+				} else {
+					val = false;
+				}
+				arrBool[i] = val;
+				Console.WriteLine(arrBool[i] + "-- " + i);
+			}
+        }
+    }
+}
